231eb104f7b718bd6f89d9d96e01c54b1e7c4047
kurata1016 <kurata1016@gmail.com>
2014-06-27 14:32:29 +0900

    first commit

---
+++ src/jp/digitalcloud/sample/twitter/auth/MainActivity.java
@@ -0,0 +1,63 @@
+package jp.digitalcloud.sample.twitter.auth;
+
+import sample.twitter.tweet.TweetActivity;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends FragmentActivity implements OnClickListener {
+
+	private Twitter mTwitter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Button buttonAuth = (Button) findViewById(R.id.buttonAuth);
+		buttonAuth.setOnClickListener(this);
+		Button buttonSendPin = (Button) findViewById(R.id.buttonSendPin);
+		buttonSendPin.setOnClickListener(this);
+		Button buttonShowTweet = (Button) findViewById(R.id.button_showTimeline);
+		buttonShowTweet.setOnClickListener(this);
+		Button buttonTweet = (Button) findViewById(R.id.button_tweet);
+		buttonTweet.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.buttonAuth:
+			mTwitter = new TwitterFactory().getInstance();
+			mTwitter.setOAuthConsumer(getString(R.string.twitter_consumer_key), getString(R.string.twitter_consumer_key_secret));
+			mTwitter.setOAuthAccessToken(null);
+			LoaderManager.LoaderCallbacks<RequestToken> requestTokenCallbacks = new TwitterOAuthRequestTokenCallbacks(this, mTwitter);
+			getSupportLoaderManager().initLoader(0, null, requestTokenCallbacks);
+			break;
+		case R.id.buttonSendPin:
+			EditText editTextPin = (EditText) findViewById(R.id.editTextPin);
+			if (editTextPin.getText().length() > 0 && mTwitter != null) {
+				LoaderManager.LoaderCallbacks<AccessToken> accessTokenCallbacks = new TwitterOAuthAccessTokenCallbacks(this, mTwitter, editTextPin.getText().toString());
+				getSupportLoaderManager().initLoader(1, null, accessTokenCallbacks);
+			}
+			break;
+		case R.id.button_showTimeline:
+			break;
+		case R.id.button_tweet:
+			Intent intent = new Intent(MainActivity.this,TweetActivity.class);
+			startActivity(intent);
+			break;
+		default:
+			break;
+		}
+	}
+}
---
+++ src/sample/twitter/tweet/TweetActivity.java
@@ -0,0 +1,106 @@
+package sample.twitter.tweet;
+
+import jp.digitalcloud.sample.twitter.auth.R;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class TweetActivity extends Activity {
+	// twitterオブジェクト
+	Twitter tw;
+	// AccessTokenオブジェクト
+	AccessToken at;
+	// AccessToken
+	String AccessToken;
+	// AccessTokenSecret
+	String AccessTokenSecret;
+	// 　ボタンタグ
+	final String BTN_BACK = "btn_back";
+	final String BTN_TWEET = "btn_tweet";
+	// 非同期タスク
+	AsyncTask<String, Void, Boolean> task;
+
+	// onCreateメソッド(画面初期表示イベント)
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// レイアウト設定ファイル指定
+		setContentView(jp.digitalcloud.sample.twitter.auth.R.layout.tweet);
+
+		// ボタンオブジェクト取得
+		Button btn_back = (Button) findViewById(R.id.btn_back);
+		btn_back.setTag(BTN_BACK);
+		btn_back.setOnClickListener(onBtnClickListener);
+		Button btn_tweet = (Button) findViewById(R.id.btn_tweet);
+		btn_tweet.setTag(BTN_TWEET);
+		btn_tweet.setOnClickListener(onBtnClickListener);
+
+		// preferenceからAccessToken取得
+		SharedPreferences pref = getSharedPreferences(this.getString(R.string.shared_pref_name), MODE_PRIVATE);
+		AccessToken = pref.getString(getString(R.string.shared_pref_key_twitter_access_token), "");
+		AccessTokenSecret = pref.getString(getString(R.string.shared_pref_key_twitter_access_token_secret), "");
+		// twitterオブジェクト取得
+		tw = new TwitterFactory().getInstance();
+		// AccessTokenオブジェクト取得
+		at = new AccessToken(AccessToken, AccessTokenSecret);
+		// APIkey と APIkeysecretの設定
+		tw.setOAuthConsumer(getString(R.string.twitter_consumer_key), getString(R.string.twitter_consumer_key_secret));
+		// AccessTokenの設定
+		tw.setOAuthAccessToken(at);
+
+		// ネットワーク通信は非同期処理
+		task = new AsyncTask<String, Void, Boolean>() {
+
+			@Override
+			protected Boolean doInBackground(String... params) {
+				// ツイート内容取得
+				EditText et = (EditText) findViewById(R.id.et_tweet);
+				// ツイート処理
+				try {
+					tw.updateStatus(et.getText().toString());
+					return true;
+				} catch (TwitterException e) {
+					e.printStackTrace();
+					return false;
+				}
+			}
+
+			@Override
+			protected void onPostExecute(Boolean tweetResult) {
+				if (tweetResult) {
+					Toast.makeText(TweetActivity.this, "ツイートしました！", Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(TweetActivity.this, "ツイートできませんでした", Toast.LENGTH_SHORT).show();
+				}
+			}
+		};
+	}
+
+	private OnClickListener onBtnClickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// ボタンオブジェクト取得
+			Button button = (Button) v;
+			// ボタン判別
+			switch (button.getTag().toString()) {
+			case BTN_BACK:
+				finish();
+				break;
+			case BTN_TWEET:
+				task.execute();
+				break;
+			}
+		}
+	};
+}
